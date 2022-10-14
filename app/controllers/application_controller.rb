4ee1261343652eea6c317ae408071296d59bc2fa
JonathanCastr <jcastro@itpro.cl>
2022-10-14 13:08:50 -0300

    commit inicial trabajo indiv 9_5

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,15 @@
+class ApplicationController < ActionController::Base
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:mail, :encrypted_password])  
+    end
+    def permitted_administrador?
+        if !current_user.administrador?
+            redirect_to etiqueta_path, alert: "No tiene permisos para editar etiquetas"
+        end
+    end
+
+end
+
---
+++ app/models/usuario.rb
@@ -0,0 +1,16 @@
+class Usuario < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  has_many :publicacions, through: :tableros
+  accepts_nested_attributes_for :publicacions 
+
+  enum rol: [:fotografo, :administrador]
+
+  after_initialize do
+    if self.new_record?
+      self.rol ||= :fotografo
+    end
+  end
+end
